a50e0bd2c4b518ebf526497d91b75328a15f1933
Vijay Patel <veejspeej@gmail.com>
2013-12-19 20:28:52 +0000

    completed impl. of wordAnagrams function

--- coursera-fp-week6-assignments/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ coursera-fp-week6-assignments/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -62,14 +62,14 @@ object Anagrams {
    *    List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
    *
    */
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = 
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = {
     for {
-        (occ, occForWords) <- dictionary.map(w => (wordOccurrences(w), w)).groupBy(t => t._1)
-    } yield occ -> occForWords.map(t => t._2)
+      (occKey, occForWords) <- dictionary.map(w => (wordOccurrences(w), w)).groupBy(t => t._1)
+    } yield occKey -> occForWords.map(t => t._2)
+  } withDefaultValue Nil
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = ???
-    
+  def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences(wordOccurrences(word))
 
   /**
    * Returns the list of all subsets of the occurrence list.
